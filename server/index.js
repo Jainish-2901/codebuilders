36416ad5e6eb62165c4f91d3e280af0392613a65
Jainish Dabgar <jainishdabgar2901@gmail.com>
2025-12-23 13:55:00 +0530

    testdeploy

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use(helmet({
 
 // 2. CORS Configuration (Dynamic)
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Use env var, fallback to localhost
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
